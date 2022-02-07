b6c735871dc06ae5827d5f96be22a35af940311a
JonathanRosado9 <48699195+JonathanRosado9@users.noreply.github.com>
2022-02-07 17:35:07 -0400

    changing icons html

--- src/views/GamePane.js
+++ src/views/GamePane.js
@@ -8,9 +8,9 @@ export default function GamePane({match, guesses=[], keyboard, handleKeyboard, t
   return (
       <div className="GamePane Pane">
           <div className="GamePaneHeader">
-                <div className="RulesButton" onClick={openRules}><span className="material-icons">question_mark</span></div>
+                <div className="RulesButton" onClick={openRules}><span className="material-icons">&#xEB8B</span></div>
                 <div className="HeaderBrand">WORDLE</div>
-                <div className="SettingsButton" onClick={openSettings}><span className="material-icons">settings</span></div>
+                <div className="SettingsButton" onClick={openSettings}><span className="material-icons">&#xE8B8</span></div>
           </div>
           <div className="GamePaneGrid">
             <WordGrid match={match} guesses={guesses} tries={tries} />
