714bef6085a53bd2b1d3d9a2f2ac727de46d806b
JonathanRosado9 <48699195+JonathanRosado9@users.noreply.github.com>
2022-02-07 17:46:23 -0400

    undo changes

--- src/views/GamePane.js
+++ src/views/GamePane.js
@@ -8,7 +8,7 @@ export default function GamePane({match, guesses=[], keyboard, handleKeyboard, t
   return (
       <div className="GamePane Pane">
           <div className="GamePaneHeader">
-                <div className="RulesButton" onClick={openRules}><span className="material-icons">question mark</span></div>
+                <div className="RulesButton" onClick={openRules}><span className="material-icons">question_mark</span></div>
                 <div className="HeaderBrand">WORDLE</div>
                 <div className="SettingsButton" onClick={openSettings}><span className="material-icons">settings</span></div>
           </div>
